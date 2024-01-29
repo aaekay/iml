8bf46f51a1698340f7c65d53c1bde5d8cc194525
Amit Kumar <aaekayg@gmail.com>
2024-01-29 19:27:21 +0530

    removed error of label name in abdomen, and removed the preparation for abdctk dataset

--- prepare_public_dataset.py
+++ prepare_public_dataset.py
@@ -1,6 +1,5 @@
 import zipfile
 import os
-import py7zr
 import sys
 import glob
 import shutil
@@ -11,16 +10,12 @@ def unzip_file(zip_file_path, extract_to_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to_path)
 
-def extract_7z_file(file_path, extract_to_path):
-    with py7zr.SevenZipFile(file_path, mode='r') as archive:
-        archive.extractall(extract_to_path)
-
 #checking the all folder files
 folder = "./public_data"
 datasets_name = os.listdir(folder)
 
 # below are the names of the datasests that you need to download
-datasets = ["Abdomen.zip", "AbdomenCT-1K-ImagePart1.zip", "AbdomenCT-1K-ImagePart2.zip", "AbdomenCT-1K-ImagePart3.zip", "Mask.7z", "Totalsegmentator_dataset_v2.zip"]
+datasets = ["Abdomen.zip", "images.zip", "label.zip" "Totalsegmentator_dataset_v2.zip"]
 
 #checking if all the datasets are downloaded or not
 for i in datasets:
@@ -31,8 +26,7 @@ for i in datasets:
 for name in datasets:
     if ".zip" in name:
         unzip_file(os.path.join(folder, name), folder)
-    if ".7z" in name:
-        extract_7z_file(os.path.join(folder, name), folder "/Masks/")
+
 
 # creating a combined folder
 combined = "./public_data/combined/"
@@ -51,9 +45,10 @@ for i in images:
     image_name = i.split("/")[-1][3:]
     os.rename(i, combined + "images/" + "abdomen_" + image_name)
 for i in labels:
-    label_name = i.split("/")[-1][6:]
+    label_name = i.split("/")[-1][5:]
     os.rename(i, combined + "labels/" + "abdomen_" + label_name)
 
+'''
 # preparing abdomen ct1k datset
 abdomenct = ["AbdomenCT-1K-ImagePart1", "AbdomenCT-1K-ImagePart2", "AbdomenCT-1K-ImagePart3", "Mask"]
 images = []
@@ -79,7 +74,7 @@ for i in labels:
     else:
         os.rename(i, combined + "labels/" + "abdctk_" + name)
         os.rename(image_dir + name, combined + "images/" + "abdctk_" + name )
-
+'''
 
 # for total segmentator dataset
 images = glob.glob("public_data/Totalsegmentator_dataset_v2/" + "/**/" "ct.nii.gz")
@@ -91,6 +86,20 @@ for i in images:
 for i in labels:
     label_name = i.split("/")[-3]
     os.rename(i, combined + "labels/" + "ts_" + label_name + ".nii.gz")
+
+# preparing miccai flare dataset
+images = glob.glob("public_data/images/" + "*.nii.gz")
+labels = glob.glob("public_data/labels/" + "*.nii.gz")
+print(len(images), len(labels))
+for i in images:
+    image_name = i.split("/")[-1][:-12]
+    #print(image_name)
+    os.rename(i, combined + "images/" + image_name + ".nii.gz")
+for i in labels:
+    label_name = i.split("/")[-1]
+    #print(label_name)
+    os.rename(i, combined + "labels/" + label_name )
+
 
 
 ## removing the directories that are not required
