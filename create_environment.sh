850aff7aa3babde0cc287ff88277496a8754e379
amit <aaekayg@gmail.com>
2024-02-02 16:45:25 +0530

    added yes to anaconda env and repaired requirements file

--- create_environment.sh
+++ create_environment.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source activate base
-conda create -n iml python=3.9
+conda create -n iml python=3.9 -y
 conda activate iml
-cd iml/
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install monai[all]==0.9.0
 pip install -r requirements.txt
